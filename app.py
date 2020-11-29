71bfac84fb0e4d5bac7ccabcc1131c071d29f916
aico_confs <aicotsu@gmail.com>
2020-11-29 23:24:26 +0800

    'ok'

--- app.py
+++ app.py
@@ -45,7 +45,21 @@ def callback():
     return 'OK'
 
 # 處理訊息
-import youtube as yt
+
+def yvideo(url = 'https://www.youtube.com/watch?v=qmeXgtzr-Xg'):
+    search_url = 'https://qdownloader.io/download?url={}'.format(r.quote(url))
+    request=r.Request(search_url, headers={"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"})
+
+    with r.urlopen(request) as response:
+        data=response.read().decode("utf-8")
+    import bs4
+    soup = bs4.BeautifulSoup(data, "html.parser")
+    t = soup.select('.col-md-8 td a' )
+    url = t[0]['href']
+    t = soup.select('.info.col-md-4 img' )
+    img = t[0]['src']
+    url = re.search(r'.*&title',url).group()[:-6]
+    return url,img
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     client_msg = event.message.text
@@ -103,7 +117,7 @@ def handle_message(event):
         # preview_image_url是外面看到的
     elif event.message.text == "影片":
 
-        line_bot_api.reply_message(event.reply_token , VideoSendMessage(original_content_url= yt.yvideo()[0], preview_image_url= yt.yvideo()[1]))
+        line_bot_api.reply_message(event.reply_token , VideoSendMessage(original_content_url= yvideo()[0], preview_image_url= yvideo()[1]))
       
 
  
--- try4.py
+++ try4.py
@@ -3,3 +3,8 @@ import youtube as yt
 print(yt.yvideo()[1])
 
 print(type(yt.yvideo()[1]))
+
+import urllib.request as r
+import re
+
+

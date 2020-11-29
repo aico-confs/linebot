3daddedb7601ec66a512b42d7dfb8f1ca66baed9
aico_confs <aicotsu@gmail.com>
2020-11-29 23:05:44 +0800

    'ok'

--- youtube.py
+++ youtube.py
@@ -1,6 +1,6 @@
 import urllib.request as r
 import re
-def yvideo(url = 'https://www.youtube.com/watch?v=qmeXgtzr-Xg'):
+def yvideo(url):
     search_url = 'https://qdownloader.io/download?url={}'.format(r.quote(url))
     # search_url = search_url.replace()
     # print(search_url)
@@ -27,6 +27,6 @@ def yvideo(url = 'https://www.youtube.com/watch?v=qmeXgtzr-Xg'):
 
 
 # search_url = 'https://www.youtube.com/results?search_query={}&sp=EgIQAQ%253D%253D'.format(r.quote(keyword))
-# print(yvideo("https://www.youtube.com/watch?v=qmeXgtzr-Xg"))
+print(yvideo("https://www.youtube.com/watch?v=qmeXgtzr-Xg")[0])
 # //ytd-video-renderer[1]/div[1]/div/div[1]/div/h3/a
 # //ytd-video-renderer[2]/div[1]/div/div[1]/div/h3/a
